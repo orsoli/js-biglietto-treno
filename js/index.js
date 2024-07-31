9ddc6dfc291e74f17b289d1af95b1bb789d53507
orsoli <fljcoli@gmail.com>
2024-07-31 15:17:55 +0200

    Added structure coment steps

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("js ok");
+/*
+    - Retrieve the interested of element on DOM
+    - Save the price for 1 km
+    - Save the discounts in decimal numbers
+    - Save in decimal precision number the price for 1km of -18 with (20% discount) 
+    - Save in decimal precision number the price for 1km of +65 with (40% discount) 
+    - Ask uer the distance of his/her trip
+    - Ask user the age
+    - Cacolate final Price
+    - Cacolate final minor Price (-18)
+    - Cacolate final sinior Price (+65)
+    - Print the final (minor/senior) price in consol in decimal precision max 2 dec places.
+    BONU
+    - Print in page
+
+*/
